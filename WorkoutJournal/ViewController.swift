c47c70a938a561ad1c30cd219ed6be44f149ae44
davetek <dlawrence4994@gmail.com>
2018-10-05 18:16:38 -0400

    Initial Commit

---
+++ WorkoutJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkoutJournal
+//
+//  Created by David Lawrence on 10/5/18.
+//  Copyright © 2018 clarity for action. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
