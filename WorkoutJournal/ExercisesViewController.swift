c284119a975c747052a30c0f439f38272a662148
davetek <dlawrence4994@gmail.com>
2018-10-06 07:26:21 -0400

    Add table view to ExercisesViewController, and set the view controller as the table view’s data source. When app is run, exercises from data source are now listed on the Exercises tab’s initial view. At this point the data store only contains an array of strings for exercise names, so only the title field is populated for each exercise.

--- WorkoutJournal/ExercisesViewController.swift
+++ WorkoutJournal/ExercisesViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ExercisesViewController: UIViewController {
+class ExercisesViewController: UIViewController, UITableViewDataSource{
     
     //set up a data variable; an instance of DataStore
     // will be injected into the variable from the AppDelegate
     var exerciseData: [String]!
     
+    @IBOutlet var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return exerciseData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exercisesPrototypeCell", for: indexPath)
+        let exerciseName = exerciseData[indexPath.row]
+        
+        cell.textLabel?.text = exerciseName
+        return cell
+    }
+    
     override func viewDidLoad() {
         super .viewDidLoad()
+        tableView.dataSource = self
     }
+    
+    
 }
