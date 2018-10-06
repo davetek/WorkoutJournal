fe5691a65beae16ce8c14fa72843c8f8d7955b42
davetek <dlawrence4994@gmail.com>
2018-10-06 09:00:23 -0400

    Add a table view to the WorkoutsViewController and configure it as done for the ExercisesViewController. Add a navigation item title to each view controller in the storyboard. When the app is run, the Workouts tab appears with the title ‘Workouts’ and shows a list of workouts, with type and date for each workout. The Exercises tab appears with the title ‘Exercises’ and shows a list of exercises, with a name and type for each exercise.

--- WorkoutJournal/WorkoutsViewController.swift
+++ WorkoutJournal/WorkoutsViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class WorkoutsViewController: UIViewController {
+class WorkoutsViewController: UIViewController, UITableViewDataSource {
     
     //set up a data variable; an instance of DataStore
     // will be injected into the variable from the AppDelegate
     var workoutData: [[String: Any]]!
     
+    @IBOutlet var tableView: UITableView!
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return workoutData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "workoutsPrototypeCell", for: indexPath)
+        let workout = workoutData[indexPath.row]
+        let workoutDate = workout["date"]
+        let workoutType = workout["type"]
+        cell.textLabel?.text = workoutType as! String
+        cell.detailTextLabel?.text = workoutDate as! String
+        
+        return cell
+    }
+    
+    
     override func viewDidLoad() {
         super .viewDidLoad()
+        tableView.dataSource = self
     }
 }
